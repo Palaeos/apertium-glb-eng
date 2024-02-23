ba03f7ece9ddd72bcb81af3752fcf101ec8e7cfb
Paleos <Paleos@paleos.me>
2024-02-23 14:47:30 +0100

    Added support for some basic lexical selection. It is generated via finding English-Spanish pairs in the word-list and replacing the selected Spanish word in the Eng-Spa with the appropriate Globasa word that translates both of them.

--- converter.py
+++ converter.py
@@ -1,15 +1,31 @@
 import xml.etree.ElementTree as ET
+import csv, re
+
 
 def find_and_override_lemma(input_file, output_file, select_tag, target_opening, target_closing, new_lemma):
     tree = ET.parse(input_file)
     root = tree.getroot()
+    EngSpaGlb_dictionary = {}
+    filename = "./word-list.csv"
+
+    with open(filename, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in spamreader:
+            englishGlosses = re.sub("\(_.*?_\)", "", row[4]).strip().split("; ")
+            spanishGlosses = re.sub("\(_.*?_\)", "", row[7]).strip().split("; ")
+            for englishGloss in englishGlosses[0].split(", "):
+                for spanishGloss in spanishGlosses[0].split(", "):
+                    EngSpaGlb_dictionary[(englishGloss, spanishGloss)] = row[0]
+
+
 
     stack = [root]
     while stack:
         current = stack.pop()
         for child in current:
             if child.tag == select_tag:
-                print(current.attrib['lemma'])
+                if (current.attrib['lemma'], child.attrib['lemma']) in EngSpaGlb_dictionary:
+                    child.attrib['lemma'] = EngSpaGlb_dictionary[current.attrib['lemma'], child.attrib['lemma']]
                 print(child.attrib['lemma'])
             stack.append(child)
 
